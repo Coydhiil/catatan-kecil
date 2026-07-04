a78cd036284f4a6eb025ff0f78b69336e490d8ff
Coydhiil <fadhiil.fiannata@gmail.com>
2026-07-04 15:01:34 +0700

    changed dependency

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 
 export default function Navbar() {
-  return (
-    <div className="flex justify-between items-center shadow-md shadow-gray-400 px-4 py-6 bg-gray-800 text-white">
-        <Link href="/">
-            <h1 className="text-xl font-bold">Fianytho.</h1>
-        </Link>
-        <div className="flex flex-row gap-8">
+    return (
+        <div className="flex justify-between items-center shadow-md shadow-gray-400 px-4 py-6 bg-gray-800 text-white">
             <Link href="/">
-                <p>Home</p>
+                <h1 className="text-xl font-bold">Fianytho.</h1>
             </Link>
-            <Link href="/projects">
-                <p>Projects</p>
-            </Link>
+            <div className="flex flex-row gap-8">
+                <Link href="/">
+                    <p>Home</p>
+                </Link>
+                <Link href="/projects">
+                    <p>Projects</p>
+                </Link>
+            </div>
         </div>
-    </div>
     );
 }
